9d60f9ee0bda450f0fac2bbd12b6ad16a8f21068
Noix2kgouu <l.crappier@gmail.com>
2022-06-23 17:26:32 +0200

    comments geolocalisation

--- geolocalisation.js
+++ geolocalisation.js
@@ -1,19 +1,19 @@
-//*Géolocalisation de l'utilisateur
+//*GÉOLOCALISATION DE L'UTILISATEUR
 
-
-let buttonGeolocalisation = document.querySelector('#geolocalisationButton');//bouton
+let buttonGeolocalisation = document.querySelector('#geolocalisationButton');//bouton geolocalisation
 
 //variable données lat, long
 let latitudeUser = null;
 let longitudeUser = null;
 
 buttonGeolocalisation.addEventListener("click", () => {
-    let textErreur = document.querySelector('#geolocationError');//message d'erreur
+    //reset message d'erreur
+    let textErreur = document.querySelector('#geolocationError');
     textErreur.style.display = "none"
-    let textErreurCoorGeo = document.querySelector('#coordGeoError');//message d'erreur
+    let textErreurCoorGeo = document.querySelector('#coordGeoError');
     textErreurCoorGeo.style.display = "none"
 
-    //si formulaire coordonnée précise visiblez alors invisible
+    //si formulaire coordonnée précise visible alors invisible
     let formulaireCoorGeo = document.querySelector('#formulaireCoordonneGeo');
     formulaireCoorGeo.style.display = "none";
 
@@ -21,21 +21,20 @@ buttonGeolocalisation.addEventListener("click", () => {
         //récupération position
         latitudeUser = position.coords.latitude;
         longitudeUser = position.coords.longitude;
-
+        
         //stockage position
         sessionStorage.setItem('latitudeUser', latitudeUser);
         sessionStorage.setItem('longitudeUser', longitudeUser);
-
+        
         location = "resultat/resultat.html";//affichage page résultat
     }
-
     const error = () => {
         console.log("Erreur de localisation");
-
+        
         let textErreur = document.querySelector('#geolocationError');//message d'erreur
         textErreur.style.display = "block"
     }
-
-    navigator.geolocation.getCurrentPosition(sucess, error);
+    
+    navigator.geolocation.getCurrentPosition(sucess, error);//demande géolocalisation
 
 })
