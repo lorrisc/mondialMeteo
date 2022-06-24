683fd3a8b9ffd1853296e69e3184c4b2009c2416
Noix2kgouu <l.crappier@gmail.com>
2022-06-24 02:29:27 +0200

    commentaire searchCity

--- searchCity.js
+++ searchCity.js
@@ -1,7 +1,11 @@
+//*SEARCH BAR
+
 let searchBar = document.querySelector("#searchBar")
 
+//*saisie clavier event
 searchBar.addEventListener("keyup", (e) => {
-    let searchLetters = e.target.value
+    let searchLetters = e.target.value;//*information saisie
+
 
     var requestOptions = {
         method: 'GET',
@@ -12,44 +16,52 @@ searchBar.addEventListener("keyup", (e) => {
         .then(result => {
             // console.log(result);
 
-            let searchResult = document.querySelector("#resultatSearch")
+            let searchResult = document.querySelector("#resultatSearch");//conteneur résultats search bar
 
-            let tablResultat = []
+            let tablResultat = []//initialisation tableau stockage résultat
 
+            //*delete all element in resutat
             let child = searchResult.lastElementChild
             while (child) {
                 searchResult.removeChild(child)
                 child = searchResult.lastElementChild
             }
 
+            //*Pour la longueur de retour de l'API
             for (i = 0; i < result.results.length; i++) {
                 let nouveauResultat = tablResultat.push([result.results[i].city, result.results[i].postcode, result.results[i].county, result.results[i].lat, result.results[i].lon])
 
-                searchResult.style.display = "flex";
+                searchResult.style.display = "flex";//Resultat visible
 
+                //*création élément 1 résultat
                 let cityContenairResult = document.createElement('div');
                 cityContenairResult.className = "resultatSearch"
-                cityContenairResult.id = i
+                cityContenairResult.id = i//id i (idem que son index dans le tableau)
                 searchResult.appendChild(cityContenairResult);
 
+                //*text
                 let cityResult = document.createElement('p');
                 cityResult.textContent = result.results[i].city + "(" + result.results[i].postcode + ", " + result.results[i].county + ")";
                 cityContenairResult.appendChild(cityResult);
             }
 
+            //*Ville sélectionné
             let cityContenairResult = document.querySelectorAll(".resultatSearch")
-            for (let i = 0; i < cityContenairResult.length; i++) {
-                cityContenairResult[i].addEventListener("click", () => {
+            for (let i = 0; i < cityContenairResult.length; i++) {//pour le nombre d'élément
+                cityContenairResult[i].addEventListener("click", () => {//un élément cliqué
+                    //*récupération latitude et longitude (tableau)
                     let latitude = tablResultat[i][3]
                     let longitude = tablResultat[i][4]
 
+                    //*sauvegarde infos
                     sessionStorage.setItem('latitudeUser', latitude);
                     sessionStorage.setItem('longitudeUser', longitude);
 
-                    location = "resultat/resultat.html";//affichage page résultat
+                    location = "resultat/resultat.html";//*affichage page résultat
                 })
             }
 
+            //*Cancel popup résultat
             document.body.addEventListener("click",()=>{
                 searchResult.style.display="none"
             })
