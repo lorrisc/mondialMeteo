b74fea3a6781a3c597389cc348c963a3d968c781
Noix2kgouu <l.crappier@gmail.com>
2022-06-24 14:15:59 +0200

    close search searchbar

--- responsiveSearchBar.js
+++ responsiveSearchBar.js
@@ -1,13 +1,39 @@
 let iconSearch = document.querySelector("#searchIcon")
+let closeIcon = document.querySelector("#closeIcon");
+
 let statusSearch = 0
 iconSearch.addEventListener("click", () => {
     let searchBar = document.querySelector("#searchBar");
-    if (statusSearch == 0) {
-        searchBar.style.display = "block";
-        statusSearch = 1
-    }
-    else if (statusSearch == 1) {
-        searchBar.style.display = "none";
-        statusSearch = 0;
-    }
+        // searchBar.style.display = "block";
+        searchBar.style.visibility = "visible";
+        searchBar.style.width = "75vw";
+
+        let firstBarre = document.querySelector("#firstBarre");
+        let secondBarre = document.querySelector("#secondBarre");
+
+        iconSearch.style.right="-20px";
+
+        closeIcon.style.visibility="visible";
+
+        firstBarre.style.transform="rotate(45deg)";
+        secondBarre.style.transform="rotate(-45deg)";
+
+})
+closeIcon.addEventListener("click", () => {
+        searchBar.style.width = "0vw";
+        searchBar.style.visibility = "hidden";
+
+        let firstBarre = document.querySelector("#firstBarre");
+        let secondBarre = document.querySelector("#secondBarre");
+
+        
+        firstBarre.style.transform="rotate(0deg)";
+        secondBarre.style.transform="rotate(0deg)";
+        
+        closeIcon.style.visibility="hidden";
+        
+        iconSearch.style.transition="all 0.45s ease-out"
+        
+        iconSearch.style.right="20px";
+
 })
